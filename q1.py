367cbcd17cfc57f49032b48a564f0194a893cd2b
noamshtiegman <Nshtiegman2025@kehillahstudent.org>
2023-05-04 09:51:37 -0700

    q1

--- q1.py
+++ q1.py
@@ -1,1 +1,20 @@
-print("hello world")
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+tables = pd.read_html("https://en.wikipedia.org/wiki/Minnesota", match = "historical Population")
+
+table = tables[0]
+
+print(table["Census"])
+print(table["Pop."])
+df = table[["Census", "Pop."]]
+df = df.iloc[0:-2].astype("int")
+
+df.plot(x = "census", y = "pop.")
+plt.show()
